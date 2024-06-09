78b8bac445128f3d0731adf34691568f5ba9eb07
Nagai Ryosuke <n831rsuke@icloud.com>
2024-06-10 04:10:46 +0900

    Initial Commit

---
+++ WakeMeUp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WakeMeUp
+//
+//  Created by 長井亮輔 on 2024/06/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WakeMeUp/WakeMeUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  WakeMeUpApp.swift
+//  WakeMeUp
+//
+//  Created by 長井亮輔 on 2024/06/10.
+//
+
+import SwiftUI
+
+@main
+struct WakeMeUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
