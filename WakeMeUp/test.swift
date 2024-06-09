56a90da764bf82d4306a13faf2d734e1f0a3e1a6
Nagai Ryosuke <n831rsuke@icloud.com>
2024-06-10 04:59:41 +0900

    test

---
+++ WakeMeUp/test.swift
@@ -0,0 +1,18 @@
+//
+//  test.swift
+//  WakeMeUp
+//
+//  Created by 長井亮輔 on 2024/06/10.
+//
+
+import SwiftUI
+
+struct test: View {
+    var body: some View {
+        Text("Nice to meet you!!")
+    }
+}
+
+#Preview {
+    test()
+}
