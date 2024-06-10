1981f5cf8d9e0ff3f3891fa84b4df716b276b0c8
Nagai Ryosuke <n831rsuke@icloud.com>
2024-06-11 01:29:18 +0900

    AppView

---
+++ WakeMeUp/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  WakeMeUp
+//
+//  Created by 長井亮輔 on 2024/06/11.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Home()
+}
---
+++ WakeMeUp/Start.swift
@@ -0,0 +1,18 @@
+//
+//  Start.swift
+//  WakeMeUp
+//
+//  Created by 長井亮輔 on 2024/06/11.
+//
+
+import SwiftUI
+
+struct Start: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Start()
+}
