e077a8c3eca0455ba2ee109c289a2dfaefd905f8
Nagai Ryosuke <n831rsuke@icloud.com>
2024-06-12 02:13:31 +0900

    単語・意味・例文

--- WakeMeUp/Pre_Mission.swift
+++ WakeMeUp/Pre_Mission.swift
@@ -9,20 +9,24 @@ import SwiftUI
 import SwiftCSV
 
 struct Pre_Mission: View {
-    @State private var randomEntry: (String, String) = ("", "")
+    @State private var randomEntry: (String, String, String) = ("", "", "")
     
     var body: some View {
         VStack {
             Spacer() // 上部スペース
-                        Text(randomEntry.0)
-                            .font(.largeTitle)
-                            .multilineTextAlignment(.center)
-                            .padding(.bottom, 10) // 下部に少し余白を追加
-                        Text(randomEntry.1)
-                            .font(.subheadline)
-                            .multilineTextAlignment(.center)
-                            .padding()
-                        Spacer() // 下部スペース
+            Text(randomEntry.0)
+                .font(.largeTitle)
+                .multilineTextAlignment(.center)
+                .padding(.bottom, 10) // 下部に少し余白を追加
+            Text(randomEntry.1)
+                .font(.subheadline)
+                .multilineTextAlignment(.center)
+                .padding()
+            Text(randomEntry.2)
+                .font(.subheadline)
+                .multilineTextAlignment(.center)
+                .padding()
+            Spacer() // 下部スペース
             
             Button(action: {
                 randomEntry = loadRandomEntry()
@@ -36,27 +40,29 @@ struct Pre_Mission: View {
         }
     }
     
-    func loadRandomEntry() -> (String, String) {
+    func loadRandomEntry() -> (String, String, String) {
         guard let csvURL = Bundle.main.url(forResource: "TOEIC", withExtension: "csv") else {
             print("CSV file not found")
-            return ("Error", "CSV file not found")
+            return ("Error", "CSV file not found", "")
         }
         
         do {
             let csv = try CSV<Named>(url: csvURL)
-            if let entries = csv.columns?["entry"] as? [String], let meanings = csv.columns?["meaning"] as? [String]{
-                let combinedEntries = zip(entries, meanings).map { ($0, $1) }
+            if let entries = csv.columns?["entry"] as? [String],
+               let meanings = csv.columns?["meaning"] as? [String],
+               let examples = csv.columns?["example_sentence"] as? [String] {
+                let combinedEntries = zip(zip(entries, meanings), examples).map { ($0.0, $0.1, $1) }
                 if let randomElement = combinedEntries.randomElement() {
                     return randomElement
                 } else {
-                    return ("No entries found", "")
+                    return ("No entries found", "", "")
                 }
             } else {
-                return ("No entries found", "")
+                return ("No entries found", "", "")
             }
         } catch {
             print("Error reading CSV file: \(error)")
-            return ("Error", "reading CSV file")
+            return ("Error", "reading CSV file", "")
         }
     }
 }
