486ce70a5ed8996f97619f434e11f15b2e922936
Nagai Ryosuke <n831rsuke@icloud.com>
2024-06-11 19:36:29 +0900

    gitignore

--- WakeMeUp/Mission.swift
+++ WakeMeUp/Mission.swift
@@ -1,18 +1,1 @@
-//
-//  Mission.swift
-//  WakeMeUp
-//
-//  Created by 長井亮輔 on 2024/06/11.
-//
 
-import SwiftUI
-
-struct Mission: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-#Preview {
-    Mission()
-}
